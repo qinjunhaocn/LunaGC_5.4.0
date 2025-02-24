05654a3d13158203b4c4c12e29073de4c4414b6d
manu.dll <114945940+manu1337-sex@users.noreply.github.com>
2025-02-24 18:19:15 +0100

    Update gameconstants, natlan abilities

--- src/main/java/emu/grasscutter/GameConstants.java
+++ src/main/java/emu/grasscutter/GameConstants.java
@@ -12,7 +12,7 @@ public final class GameConstants {
     public static boolean DEBUG = false;
 
     public static final int DEFAULT_TEAMS = 4;
-    public static final int MAX_TEAMS = 10;
+    public static final int MAX_TEAMS = 50;
     public static final int MAIN_CHARACTER_MALE = 10000005;
     public static final int MAIN_CHARACTER_FEMALE = 10000007;
     public static final Position START_POSITION = new Position(2747, 194, -1719);
@@ -27,28 +27,75 @@ public final class GameConstants {
     // Default entity ability hashes.
     public static final String[] DEFAULT_ABILITY_STRINGS = {
         "Avatar_DefaultAbility_VisionReplaceDieInvincible",
-        "Avatar_DefaultAbility_AvartarInShaderChange",
-        "Avatar_SprintBS_Invincible",
-        "Avatar_Freeze_Duration_Reducer",
-        "Avatar_Attack_ReviveEnergy",
-        "Avatar_Component_Initializer",
-        "Avatar_FallAnthem_Achievement_Listener",
-        "GrapplingHookSkill_Ability",
-        "Avatar_PlayerBoy_DiveStamina_Reduction",
-        "Ability_Avatar_Dive_SealEcho",
-        "Absorb_SealEcho_Bullet_01",
-        "Absorb_SealEcho_Bullet_02",
-        "Ability_Avatar_Dive_CrabShield",
-        "ActivityAbility_Absorb_Shoot",
-        "SceneAbility_DiveVolume",
-        "Avatar_PlayerGirl_DiveStamina_Reduction",
-        "Ability_Avatar_Dive_Team",
-        "Avatar_Absorb_TrackingMissile",
-        "Avatar_Absorb_SwordFishSlash",
-    };
-    public static final String[] DEFAULT_TEAM_ABILITY_STRINGS = {
-            "Ability_Avatar_Dive_Team"
-    };
+         "Avatar_DefaultAbility_AvartarInShaderChange",
+         "Avatar_SprintBS_Invincible",
+         "Avatar_Freeze_Duration_Reducer",
+         "Avatar_Attack_ReviveEnergy",
+         "Avatar_Component_Initializer",
+         "Avatar_FallAnthem_Achievement_Listener",
+         "GrapplingHookSkill_Ability",
+         "Avatar_PlayerBoy_DiveStamina_Reduction",
+         "Ability_Avatar_Dive_SealEcho",
+         "Absorb_SealEcho_Bullet_01",
+         "Absorb_SealEcho_Bullet_02",
+         "Ability_Avatar_Dive_CrabShield",
+         "ActivityAbility_Absorb_Shoot",
+         "SceneAbility_DiveVolume",
+         "Avatar_PlayerGirl_DiveStamina_Reduction",
+         "Ability_Avatar_Dive_Team",
+         "Avatar_Absorb_TrackingMissile",
+         "Avatar_Absorb_SwordFishSlash",
+         "TeamAbility_Natsaurus_Transfer_Vehicle_Skill",
+         "DynamicAbility_Phlogiston",
+         "DynamicAbility_NightsoulBlessing",
+         "TeamAbility_Natsaurus_Vehicle_State_Listener",
+         "DynamicAbility_ArcLight_Predicate",
+         "DynamicAbility_CommonArcLight_Invincible_V5_0",
+         "DynamicAbility_ArcLight_Wathcer",
+         "SceneObj_Area_Nt_Property_Prop_YouLieDragon_Collision_DynamicAbility",
+         "TeamAbility_Natsaurus_Preload_Drillhead",
+         "TeamAbility_Natsaurus_Preload_Hookwalker",
+         "TeamAbility_Natsaurus_Preload_Mosasaurus",
+         "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet",
+         "TeamAbility_NightsoulBurst",
+         "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_01",
+         "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_02",
+         "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_03",
+         "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_04",
+         "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_05",
+         "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_06",
+         "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_07",
+         "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_5001",
+         "Avatar_HDMesh_Controller",
+         "Avatar_Trampoline_Jump_Controller",
+         "Avatar_ArkheGrade_CD_Controller",
+         "Avatar_FluidAgitator",
+         "Avatar_TriggerNyxInstant",
+         "Avatar_NyxState_Listener"
+     };
+      public static final String[] DEFAULT_TEAM_ABILITY_STRINGS = {
+             "Ability_Avatar_Dive_Team",
+             // "Vehicle_Natsaurus_Common_HumanDragonCollaboration",
+             "TeamAbility_Natsaurus_Transfer_Vehicle_Skill",
+             "DynamicAbility_Phlogiston",
+             "TeamAbility_Natsaurus_Vehicle_State_Listener",
+             "DynamicAbility_ArcLight_Predicate",
+             "DynamicAbility_CommonArcLight_Invincible_V5_0",
+             "DynamicAbility_ArcLight_Wathcer",
+             "SceneObj_Area_Nt_Property_Prop_YouLieDragon_Collision_DynamicAbility",
+             "TeamAbility_Natsaurus_Preload_Drillhead",
+             "TeamAbility_Natsaurus_Preload_Hookwalker",
+             "TeamAbility_Natsaurus_Preload_Mosasaurus",
+             "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet",
+             "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_01",
+             "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_02",
+             "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_03",
+             "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_04",
+             "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_05",
+             "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_06",
+             "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_07",
+             "TeamAbility_Natsaurus_Hookwalker_ElementalArt_TriggerBullet_5001"
+     };
     public static final SparseSet ILLEGAL_WEAPONS = new SparseSet("""
         10000-10008, 11411, 11506-11508, 12505, 12506, 12508, 12509,
         13503, 13506, 14411, 14503, 14505, 14508, 15504-15506
