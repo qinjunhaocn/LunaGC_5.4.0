6da5b06250b7d2420f0942dc7383b331ada02520
manu.dll <114945940+manu1337-sex@users.noreply.github.com>
2025-03-29 22:48:20 +0100

    sethos's energy usage. weird ass ability

--- src/main/java/emu/grasscutter/game/ability/actions/ActionReviveElemEnergy.java
+++ src/main/java/emu/grasscutter/game/ability/actions/ActionReviveElemEnergy.java
@@ -17,28 +17,33 @@ public final class ActionReviveElemEnergy extends AbilityActionHandler {
     @Override
     public boolean execute(
             Ability ability, AbilityModifierAction action, ByteString abilityData, GameEntity target) {
-                Grasscutter.getLogger().info("ReviveElemEnergy");
+        Grasscutter.getLogger().info("ReviveElemEnergy");
         float ratio = action.ratio.get(ability);
-        if (target instanceof EntityAvatar avatar) {
-        float curEnergy = avatar.getFightProperty(avatar.getAvatar().getSkillDepot().getElementType().getCurEnergyProp());
-        float newEnergy;
-        if (ratio < 0) {
-            newEnergy = curEnergy - ratio; // -(-x) = +x → Falls negativ, dann addieren
-        } else {
-            newEnergy = curEnergy + ratio; // Normaler Additionsfall
-        }
-    
-    
-        avatar.getAvatar().setCurrentEnergy(avatar.getAvatar().getSkillDepot().getElementType().getCurEnergyProp(), newEnergy);
-        avatar.getScene().broadcastPacket(new PacketEntityFightPropUpdateNotify(avatar, avatar.getAvatar().getSkillDepot().getElementType().getCurEnergyProp()));
-        avatar.getScene().broadcastPacket(new PacketEntityFightPropChangeReasonNotify(avatar, avatar.getAvatar().getSkillDepot().getElementType().getCurEnergyProp(), newEnergy, PropChangeReason.PROP_CHANGE_REASON_ABILITY, ChangeEnergyReason.CHANGE_ENERGY_REASON_ABILITY));
-        Grasscutter.getLogger().info("Revived avatar energy by " + ratio);
 
-        if (action == null) {
-            return false;
-        }
+        if (target instanceof EntityAvatar avatar) {
+            float curEnergy = avatar.getFightProperty(avatar.getAvatar().getSkillDepot().getElementType().getCurEnergyProp());
+            float newEnergy = curEnergy; 
+
+            if (ratio < 0) {
+                if (avatar.getAvatar().getAvatarId() == 10000097) {
+                    newEnergy = curEnergy + ratio; 
+                } else {
+                    newEnergy = curEnergy - ratio;
+                }
+            } else if (ratio > 0) {
+                newEnergy = curEnergy + ratio;
+            }
+
+
+            avatar.getAvatar().setCurrentEnergy(avatar.getAvatar().getSkillDepot().getElementType().getCurEnergyProp(), newEnergy);
+            avatar.getScene().broadcastPacket(new PacketEntityFightPropUpdateNotify(avatar, avatar.getAvatar().getSkillDepot().getElementType().getCurEnergyProp()));
+            avatar.getScene().broadcastPacket(new PacketEntityFightPropChangeReasonNotify(avatar, avatar.getAvatar().getSkillDepot().getElementType().getCurEnergyProp(), newEnergy, PropChangeReason.PROP_CHANGE_REASON_ABILITY, ChangeEnergyReason.CHANGE_ENERGY_REASON_ABILITY));
+            Grasscutter.getLogger().info("Revived avatar energy by " + ratio);
+            
     
-    }
-        return true;
+            return true;
+        }
+        
+        return false;
     }
 }
