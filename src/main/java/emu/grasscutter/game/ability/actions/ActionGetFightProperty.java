e1db1e96fc8e079e63fbb2921b2db5e8e1fa44cc
manu.dll <114945940+manu1337-sex@users.noreply.github.com>
2025-04-12 18:32:45 +0200

    FIGHT_PROP 到 abilitySpecials 实现

---
+++ src/main/java/emu/grasscutter/game/ability/actions/ActionGetFightProperty.java
@@ -0,0 +1,41 @@
+package emu.grasscutter.game.ability.actions;
+
+import com.google.protobuf.ByteString;
+import emu.grasscutter.data.binout.AbilityModifier.AbilityModifierAction;
+import emu.grasscutter.game.ability.Ability;
+import emu.grasscutter.game.entity.GameEntity;
+import emu.grasscutter.game.props.FightProperty;
+import it.unimi.dsi.fastutil.objects.Object2FloatOpenHashMap;
+import emu.grasscutter.Grasscutter;
+
+@AbilityAction(AbilityModifierAction.Type.GetFightProperty)
+public final class ActionGetFightProperty extends AbilityActionHandler {
+    @Override
+    public boolean execute(
+            Ability ability, AbilityModifierAction action, ByteString abilityData, GameEntity target) {
+
+        var owner = ability.getOwner();
+        var properties = new Object2FloatOpenHashMap<String>();
+
+
+        for (var property : FightProperty.values()) {
+            String name = property.name();
+            float value = owner.getFightProperty(property);
+            properties.put(name, value);
+        }
+        
+
+        properties.putAll(ability.getAbilitySpecials());
+
+
+        float fightPropertyValue = action.fightProp.get(properties, 0f);
+        
+
+        target.getGlobalAbilityValues().put(action.globalValueKey, fightPropertyValue);
+        
+        Grasscutter.getLogger().info("Set global value {} to {} (fight prop: {})", 
+                action.globalValueKey, fightPropertyValue, action.fightProp);
+        
+        return true;
+    }
+}
