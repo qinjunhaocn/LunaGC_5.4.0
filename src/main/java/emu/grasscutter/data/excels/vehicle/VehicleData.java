4ae03d9d8b799c58a88ec549caf909ce662b3c79
manu.dll <114945940+manu1337-sex@users.noreply.github.com>
2025-04-23 12:36:24 +0200

    Balls

---
+++ src/main/java/emu/grasscutter/data/excels/vehicle/VehicleData.java
@@ -0,0 +1,21 @@
+package emu.grasscutter.data.excels.avatar;
+
+import emu.grasscutter.data.*;
+import emu.grasscutter.data.ResourceType.LoadPriority;
+import emu.grasscutter.data.common.PropGrowCurve;
+import emu.grasscutter.game.props.*;
+import emu.grasscutter.utils.Utils;
+import it.unimi.dsi.fastutil.ints.*;
+import lombok.Getter;
+
+import java.util.*;
+
+@ResourceType(name = "VehicleExcelConfigData.json", loadPriority = LoadPriority.LOW)
+public class VehicleData extends GameResource {
+@Getter(onMethod_ = @Override)
+private int id;
+@Getter
+private int featureTagGroupID;
+
+                }
+       
