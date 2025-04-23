2ebd50979c10a35b1a5eee2288a00584855e9513
manu.dll <114945940+manu1337-sex@users.noreply.github.com>
2025-04-23 14:52:43 +0200

    stuff for client and other stuff

--- src/main/java/emu/grasscutter/data/binout/AbilityMixinData.java
+++ src/main/java/emu/grasscutter/data/binout/AbilityMixinData.java
@@ -1,12 +1,15 @@
 package emu.grasscutter.data.binout;
 
 import com.google.gson.*;
+
+import emu.grasscutter.data.binout.AbilityModifier.AbilityModifierAction;
 import emu.grasscutter.data.common.DynamicFloat;
 
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.reflect.TypeToken;
+
+import java.io.Serial;
 import java.io.Serializable;
-import java.lang.constant.DynamicCallSiteDesc;
 import java.util.*;
 
 public class AbilityMixinData implements Serializable {
@@ -15,15 +18,35 @@ public class AbilityMixinData implements Serializable {
     public enum Type {
         AttachToGadgetStateMixin,
         AttachToStateIDMixin,
+        SetGadgetStateV2,
         ShieldBarMixin,
+        AvatarCombatMixin,
+        DoActionByEventMixin,
+        DoActionByKillingMixin,
+        SkillButtonHoldChargeMixin,
+        GlobalSubShieldMixin,
+        TileAttackMixin,
         SwitchHealToHPDebtsMixin,
         AttachModifierToGlobalValueMixin,
+        DJLJLPAFPGN,
+        KENHGCLICPB,
         DoActionOnGlobalValueChangeMixin,
         CurLocalAvatarMixin,
         NyxCostMixin,
         ModifyDamageMixin,
         AvatarChangeSkillMixin,
+        KHOENFHDFJE,
+        HJKDMEOOBDK,
+        FIGCOCJJHCH,
+        DMKDPHHJENO,
+        LAAJCBLNLDO,
+        CameraBlurMixin,
+        AttachToNormalizedTimeMixin,
+        AttachToMultiNormalizedTimeMixin,
+        DLJBCMKDMEK,
         PhlogistonCostMixin,
+        FIHACJPNNED,
+        JMEOJHGPNMB,
         AttachModifierToSelfGlobalValueMixin,
         AttachActionToModifierMixin,
         AttachModifierToSelfGlobalValueNoInitMixin,
@@ -35,25 +58,78 @@ public class AbilityMixinData implements Serializable {
         RejectAttackMixin,
         DoActionByTargetsCountMixin,
         AttachToAbilityStateMixin,
+        IPIBBIDFDOL,
+        EBBCNBHOAIP,
+        ReviveElemEnergyMixin,
+        HKGPGGJAKGL,
+        ENPGGGNLLJG,
+        PBOJOFIGPIC,
+        IEABBMGDJHC,
         ModifyBeHitDamageMixin,
+        DoActionByCreateGadgetMixin,
         MuteHitEffectMixin,
         EntityInVisibleMixin,
+        DDCOPGJBHLB,
+        IBAMBHPLNNA,
+        PCKKGOMJIKL,
         TriggerPostProcessEffectMixin,
-        DoActionByKillingMixin,
+        JGOOOFOCJBI,
+        OOAMMMJMKPD,
+        EFDAMNIDHDC,
+        JMJFEPHFFFN,
+        AttachToAnimatorStateIDMixin,
         AvatarSteerByCameraMixin,
         ModifyDamageCountMixin,
-        OnAvatarUseSkillMixin,
-        
+        AttackCostElementMixin,
+        OnAvatarUseSkillMixin
     }
+    public AbilityModifierAction[] idontknowwhattonamethis;
+    public AbilityModifierAction[] idontknowwhattonamethis2;
+
+    @SerializedName("onEnterCombat")
+    public AbilityModifierAction[] onEnterCombat;
+
+    @SerializedName("onExitCombat")
+    public AbilityModifierAction[] onExitCombat;
+
+    @SerializedName("onTriggerSkill")
+    public AbilityModifierAction[] onTriggerSkill;
+
+    @SerializedName("onTriggerUltimateSkill")
+    public AbilityModifierAction[] onTriggerUltimateSkill;
+
+    public AbilityModifierAction[] IOKPLLOKGGJ;
+    
+    @SerializedName("onKill")
+    public AbilityModifierAction[] onKill;
+    
+    @SerializedName("successActions")
+    public AbilityModifierAction[] successActions;
+
+    @SerializedName("succActions")
+    public AbilityModifierAction[] succActions;
 
     @SerializedName("$type")
     public Type type;
 
-    private JsonElement modifierName;
+    public JsonElement modifierName;
+
+
     public DynamicFloat speed = DynamicFloat.ZERO;
     public DynamicFloat costStaminaDelta = DynamicFloat.ZERO;
     public DynamicFloat ratio = DynamicFloat.ONE;
-
+    public DynamicFloat detectWindow = DynamicFloat.ONE;
+    public String globalValueKey;
+    public List<String> stateIDs = new ArrayList<>();
+    public String stateID;
+    public DynamicFloat defaultGlobalValueOnCreate = DynamicFloat.ZERO;
+    public List<DynamicFloat> ratioSteps = new ArrayList<>();
+    public List<String> modifierNameSteps = new ArrayList<>();
+    public boolean EJEMBMFPBKF = true;
+    public boolean isCheckOnAttach = true;
+    public boolean AMFABNCKJNG = true;
+    public boolean forceStopWhenRemoved = true;
+    public boolean FKAJIEOFOAB = true;
     public List<String> getModifierNames() {
         if (modifierName.isJsonArray()) {
             java.lang.reflect.Type listType = (new TypeToken<List<String>>() {}).getType();
